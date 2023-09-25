5fc00801767124ed42557848fce3872d86df7b25
Edwin Scharfe <19590601+HippieHosenHustler@users.noreply.github.com>
2023-09-25 23:14:59 +0200

    add a nice colorscheme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,6 +44,9 @@ return packer.startup(function(use)
     use "nvim-lua/popup.nvim"       -- An implementation of the Popup API from vim in Neovim
     use "nvim-lua/plenary.nvim"     -- Useful lua functions used by lots of plugins
 
+    use "lunarvim/colorschemes"     -- A bunch of colorschemesuse { "catppuccin/nvim", as = "catppuccin" }
+    use { "catppuccin/nvim", as = "catppuccin" }
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
